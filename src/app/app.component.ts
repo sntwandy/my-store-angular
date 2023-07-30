0cad2eb134a5e2c141f1d3b3886c71b8938f52dc
sntwandy <sntwandy@gmail.com>
2023-07-30 09:19:31 -0400

    feat: Learning about event binding, string interpolation and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Megan';
+  age = 23;
+  img = 'https://source.unsplash.com/random';
+  btnDisabled = false;
+
+  person = {
+    name: 'Megan Herrera',
+    age: 23,
+    avatar: 'https://source.unsplash.com/random'
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled
+  }
+
+  increaseAge() {
+    this.person.age += 1
+  }
 }
